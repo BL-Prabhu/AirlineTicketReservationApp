f446beb64a8c7fd131ef2c14dbdc9b947ed4d496
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-07-02 17:35:32 +0530

    [Prabhu] Added : UC5-Booking Confirmed State

--- AirlineTicketReservation/src/booking/ConfirmedState.java
+++ AirlineTicketReservation/src/booking/ConfirmedState.java
@@ -1,8 +1,20 @@
 package booking;
 
 public class ConfirmedState implements BookingState {
+
+    @Override
     public void next(Booking booking) {
-        System.out.println("Booking already confirmed.");
+        System.out.println("Booking already CONFIRMED. No further state.");
     }
-    public String getStatus() { return "CONFIRMED"; }
+
+    @Override
+    public void handle() {
+        System.out.println("Booking State: CONFIRMED");
+    }
+
+    @Override
+    public String getStatus() {
+        return "CONFIRMED";
+    }
 }
+
