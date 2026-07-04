4750806a6b020dfd1a4970afee3f3b0d2c207d8c
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-07-04 11:54:19 +0530

    [Prabhu] Added : UC11-Flight Enum Domain

---
+++ AirlineTicketReservation/src/domain/flight/FlightEnums.java
@@ -0,0 +1,8 @@
+package domain.flight;
+
+public class FlightEnums {
+    public enum TravelClass { ECONOMY, PREMIUM_ECONOMY, BUSINESS, FIRST }
+    public enum TimeSlot { MORNING, AFTERNOON, EVENING, NIGHT }
+    public enum PriceRange { BUDGET, MODERATE, PREMIUM }
+    public enum FlightStatus { ON_TIME, DELAYED, CANCELLED, BOARDING }
+}
